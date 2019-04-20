2280eade05da76fc552e0a7bf5ac3ccaaf5a07d4
siva chaitanya <sivac88@gmail.com>
2019-04-20 11:48:06 +0530

    changed port number again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
